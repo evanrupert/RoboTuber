1555afd4ce40f4b9bb0d781b7908e5a9d9e9c1c9
Evan Rupert <evan@studioreach.io>
2021-11-13 14:00:17 -0500

    Restructure directory to include client code

--- server/lib/udp_server.ex
+++ server/lib/udp_server.ex
@@ -16,15 +16,17 @@ defmodule UDPServer do
   end
 
   def handle_packet(data, socket) do
-    IO.inspect data
+    # IO.inspect data
 
     <<frame::big-integer-unsigned-size(32),
       right_eye_open::integer-unsigned-size(8),
       left_eye_open::integer-unsigned-size(8),
-      mouth_open::big-float-size(32),
-      mouth_wide::big-float-size(32)>> = data
+      tvec_x::big-float-size(32),
+      tvec_y::big-float-size(32),
+      tvec_z::big-float-size(32),
+      mouth_open::big-float-size(32)>> = data
 
-      IO.puts "Frame: #{frame}, Right Eye: #{right_eye_open}, Left Eye: #{left_eye_open}, Mouth Open: #{mouth_open}, Mouth Wide: #{mouth_wide}"
+    IO.puts "Frame: #{frame}, Right Eye: #{right_eye_open}, Left Eye: #{left_eye_open}, TVec.X: #{tvec_x}, TVec.Y: #{tvec_y}, TVec.Z: #{tvec_z} Mouth Open: #{mouth_open}"
 
     {:noreply, socket}
   end
